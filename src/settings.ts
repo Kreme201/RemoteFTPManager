99f44dd5aa2c831a108dc203ca7e2aca459a6ecd
Kreme201 <kreme201@gmail.com>
2017-11-03 10:04:37 +0900

    modify sessionhandler, add init function with sample session item

--- src/settings.ts
+++ src/settings.ts
@@ -5,10 +5,15 @@ import * as os from 'os';
 
 export interface Session {
 
-    name: string;     // the name that the user defines for the Session
-    rootPath: string; // the root path of this Session
-    paths: string[];  // the 'other paths' when you have multifolder Session
-    group: string;    // the group(s) that it belongs to
+    name: string;
+    type: string;
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    remote_path: string;
+    connect_timeout: number;
+    localpath: string;
 
 };
 
@@ -16,17 +21,27 @@ export interface SessionList extends Array<Session> {};
 
 class SessionItem implements Session {
 
-    public name: string;     // the name that the user defines for the Session
-    public rootPath: string; // the root path of this Session
-    public paths: string[];  // the 'other paths' when you have multifolder Session
-    public group: string;    // the group(s) that it belongs to
+    public name: string;
+    public type: string;
+    public host: string;
+    public port: number;
+    public username: string;
+    public password: string;
+    public remote_path: string;
+    public connect_timeout: number;
+    public localpath: string;
 
-    constructor( pname: string, prootPath: string ) {
+    constructor ( p_name: string, p_type: string, p_host: string, p_port: number, p_username: string, p_password: string, p_remote_path: string, p_connect_timeout: number, p_localpath: string ) {
 
-        this.name = pname;
-        this.rootPath = prootPath;
-        this.paths = [];
-        this.group = "";
+        this.name            = p_name;
+        this.type            = p_type;
+        this.host            = p_host;
+        this.port            = p_port;
+        this.username        = p_username;
+        this.password        = p_password;
+        this.remote_path     = p_remote_path;
+        this.connect_timeout = p_connect_timeout;
+        this.localpath       = p_localpath;
 
     }
 }
@@ -38,41 +53,21 @@ export class SessionHandler {
 
     constructor () {
 
-        this.filename = this.getFileName();
-        this.sessionList   = <SessionList> [];
+        this.filename    = this.getFileName();
+        this.sessionList = <SessionList> [];
 
 
-        if ( ! fs.existsSync( this.filename ) ) {
-
-            this.save();
-
-        }
+        this.init();
 
     }
 
-    /**
-     * Adds a Session to the list
-     *
-     * @param `name` The [Session Name](#Session.name)
-     * @param `rootPath` The [Session Rooth Path](#Session.rootPath)
-     * @param `rootPath` The [Session Group](#Session.group)
-     *
-     * @return `void`
-     */
-    public push( name: string, rootPath: string, group: string ): void {
+    public push( name: string, type: string, host: string, port: number, username: string, password: string, remote_path: string, connect_timeout: number, localpath: string ): void {
 
-        this.sessionList.push( new SessionItem( name, rootPath ) );
+        this.sessionList.push(new SessionItem( name, type, host, port, username, password, remote_path, connect_timeout, localpath ) );
         return;
 
     }
 
-    /**
-     * Removes a Session to the list
-     *
-     * @param `name` The [Session Name](#Session.name)
-     *
-     * @return The [Session](#Session) that was removed
-     */
     public pop( name: string ): Session {
 
         for ( let index = 0; index < this.sessionList.length; index++ ) {
@@ -89,90 +84,6 @@ export class SessionHandler {
 
     }
 
-    /**
-     * Adds another `path` to a Session
-     *
-     * @param `name` The [Session Name](#Session.name)
-     * @param `path` The [Session Path](#Session.paths)
-     *
-     * @return `void`
-     */
-    public addPath( name: string, path: string ): void {
-
-        for ( let element of this.sessionList ) {
-
-            if ( element.name.toLowerCase() === name.toLowerCase() ) {
-
-                element.paths.push( path );
-
-            }
-
-        }
-
-    }
-
-    /**
-     * Updates the `rootPath` of a Session
-     *
-     * @param `name` The [Session Name](#Session.name)
-     * @param `name` The [Session Root Path](#Session.rootPath)
-     *
-     * @return `void`
-     */
-    public updateRootPath( name: string, path: string ): void {
-
-        for ( let element of this.sessionList ) {
-
-            if ( element.name.toLowerCase() === name.toLowerCase() ) {
-
-                element.rootPath = path;
-
-            }
-
-        }
-
-    }
-
-    /**
-     * Removes a `path` from a Session
-     *
-     * @param `name` The [Session Name](#Session.name)
-     * @param `path` The [Session Path](#Session.paths)
-     *
-     * @return `void`
-     */
-    public removePath( name: string, path: string ): void {
-
-        for ( let element of this.sessionList ) {
-
-            if ( element.name.toLowerCase() === name.toLowerCase() ) {
-
-                for ( let indexPath = 0; indexPath < element.paths.length; indexPath++ ) {
-
-                    let elementPath = element.paths[ indexPath ];
-
-                    if ( elementPath.toLowerCase() === path.toLowerCase() ) {
-
-                        element.paths.splice( indexPath, 1 );
-                        return;
-
-                    }
-
-                }
-
-            }
-
-        }
-
-    }
-
-    /**
-     * Checks if exists a Session with a given `name`
-     *
-     * @param `name` The [Session Name](#Session.name) to search for Sessions
-     *
-     * @return `true` or `false`
-     */
     public exists( name: string ): boolean {
 
         let found: boolean = false;
@@ -190,52 +101,20 @@ export class SessionHandler {
         return found;
     }
 
-    /**
-     * Checks if exists a Session with a given `rootPath`
-     *
-     * @param `rootPath` The path to search for Sessions
-     *
-     * @return A [Session](#Session) with the given `rootPath`
-     */
-    /*
-    public existsWithRootPath(rootPath: string): Session {
-        let rootPathUsingHome: string = PathUtils.compactHomePath(rootPath).toLocaleLowerCase();
-
-        for (let element of this.sessionList) {
-            if ((element.rootPath.toLocaleLowerCase() === rootPath.toLocaleLowerCase()) || (element.rootPath.toLocaleLowerCase() === rootPathUsingHome)) {
-                return element;
-            }
-        }
-    }
-    */
-
-    /**
-     * Returns the number of Sessions stored in `remote_ftp_manager.json`
-     *
-     * > The _dynamic Sessions_ like VSCode and Git aren't present
-     *
-     * @return The number of Sessions
-     */
     public length(): number {
 
         return this.sessionList.length;
 
     }
 
-    /**
-     * Loads the `remote_ftp_manager.json` file
-     *
-     * @return A `string` containing the _Error Message_ in case something goes wrong.
-     *         An **empty string** if everything is ok.
-     */
     public load(): string {
 
         let items = [];
 
-        // missing file (new install)
         if ( ! fs.existsSync( this.filename ) ) {
 
             this.sessionList = items as SessionList;
+            this.save();
             return '';
 
         }
@@ -243,22 +122,7 @@ export class SessionHandler {
         try {
 
             items = JSON.parse( fs.readFileSync( this.filename ).toString());
-
-            if ( ( items.length > 0 ) && ( items[ 0 ].label ) ) { // OLD format
-
-                for ( let element of items ) {
-
-                    this.sessionList.push( new SessionItem( element.label, element.description ) );
-
-                }
-
-                this.save(); // save updated
-
-            } else { // NEW format
-
-                this.sessionList = items as SessionList;
-
-            }
+            this.sessionList = items as SessionList;
 
             return '';
 
@@ -271,21 +135,15 @@ export class SessionHandler {
 
     }
 
-    /**
-     * Reloads the `remote_ftp_manager.json` file.
-     *
-     * > Using a forced _reload_ instead of a _watcher_
-     *
-     * @return `void`
-     */
     public reload() {
 
         let items = [];
 
 
-        if ( ! fs.existsSync( this.filename ) ) { // missing file (new install)
+        if ( ! fs.existsSync( this.filename ) ) {
 
             this.sessionList = items as SessionList;
+            this.save();
 
         } else {
 
@@ -296,22 +154,12 @@ export class SessionHandler {
 
     }
 
-    /**
-     * Saves the `remote_ftp_manager.json` file to disk
-     *
-     * @return `void`
-     */
     public save() {
 
         fs.writeFileSync( this.filename, JSON.stringify( this.sessionList, null, '\t' ) );
 
     }
 
-    /**
-     * Maps the Sessions to be used by a `showQuickPick`
-     *
-     * @return A list of Sessions `{[label, description]}` to be used on a `showQuickPick`
-     */
     public map(): any {
 
         let newItems = this.sessionList.map( item => {
@@ -319,7 +167,7 @@ export class SessionHandler {
             return {
 
                 label: item.name,
-                description: item.rootPath
+                description: item.host
 
             };
 
@@ -336,6 +184,18 @@ export class SessionHandler {
             vscode.window.showTextDocument( doc );
 
         } );
+
+    }
+
+    private init() {
+
+        if ( ! fs.existsSync( this.filename ) || 1 ) {
+
+            // set setting file with sample item
+            this.push( 'name', 'type', 'host', 21, 'username', 'password', 'remote_path', 30, 'localpath' );
+            this.save();
+
+        }
 
     }
 
